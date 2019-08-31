62803584bab8012ffd6383d7467191a04bb6ec63
tingkai-zhang <tingkai.zhang@outlook.com>
2019-08-31 14:44:45 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,13 +44,9 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
               'sphinx.ext.autosummary',
               'sphinx.ext.coverage',
-              'sphinx.ext.napoleon',
               'numpydoc'
 ]
 
-napoleon_google_docstring = False
-napoleon_use_param = False
-napoleon_use_ivar = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
