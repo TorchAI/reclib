11b8bb0b4c56eb71ad174c9ea94e1416cb9fa258
Tingkai Zhang <tingkai.zhang@outlook.com>
2019-09-30 14:29:08 -0400

    fix bugs

--- reclib/models/fnfm.py
+++ reclib/models/fnfm.py
@@ -3,7 +3,7 @@ import torch
 from reclib.models import LogisticRegression
 from reclib.modules import FeedForward
 from reclib.modules import FieldAwareFactorizationLayer
-
+from reclib.modules.embedders import LinearEmbedder
 
 class FieldAwareNeuralFactorizationMachine(torch.nn.Module):
     """
@@ -16,24 +16,25 @@ class FieldAwareNeuralFactorizationMachine(torch.nn.Module):
 
     def __init__(self, field_dims, embed_dim, mlp_dims, dropouts):
         super().__init__()
-        self.linear = LogisticRegression(field_dims)
-        self.ffm = torch.nn.Sequential(
-            FieldAwareFactorizationLayer(field_dims, embed_dim),
-            torch.nn.BatchNorm1d(embed_dim),
-            torch.nn.Dropout(dropouts[0])
-        )
+        self.linear = LinearEmbedder(field_dims)
+        self.ffm = FieldAwareFactorizationLayer(field_dims, embed_dim)
+        self.bn = torch.nn.BatchNorm1d(embed_dim),
+        self.dp = torch.nn.Dropout(dropouts[0])
+
         self.ffm_output_dim = len(field_dims) * (len(field_dims) - 1) // 2 * embed_dim
         self.mlp = FeedForward(2,
                                self.ffm_output_dim,
                                [mlp_dims, 1],
-                               True,
-                               ['relu', 'linear'],
-                               [dropouts[1], 0])
+                               batch_norm=True,
+                               activations=['relu', 'linear'],
+                               dropouts=[dropouts[1], 0])
 
     def forward(self, x):
         """
         :param x: Long tensor of size ``(batch_size, num_fields)``
         """
-        cross_term = self.ffm(self.embed_layer(x))
-        x = self.linear(x) + self.mlp(cross_term.view(-1, self.ffm_output_dim))
+        cross_term = self.ffm(x).view(-1, self.ffm_output_dim)
+        cross_term = self.bn(cross_term)
+        cross_term = self.dropout(cross_term)
+        x = self.linear(x) + self.mlp(cross_term)
         return torch.sigmoid(x.squeeze(1))
